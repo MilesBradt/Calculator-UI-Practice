4ff30a183286ea2d3638517ac08f800c7391c71c
MilesBradt <smbradtmichael@gmail.com>
2018-08-08 15:07:29 -0700

    crate addition calculator with minimal UI

---
+++ Desktop/Calculator-UI-practice/js/scripts.js
@@ -0,0 +1,62 @@
+//BMI Calculator
+// var bmi = function(weight, height){
+//   return  weight / (height**2) * 703
+// }
+//
+// var weight = parseInt(prompt("weight"));
+// var height = parseInt(prompt("height"));
+//
+// var result = bmi(weight, height);
+//
+// alert(result);
+
+//Celsius to Freedom
+// var fahrenheit = function(celsius){
+//   return celsius * (9/5) + 32
+// };
+//
+// var celsius = parseInt(prompt("Temp in celsius, please: "));
+//
+// var result = fahrenheit(celsius);
+//
+// alert(result);
+
+//Cooking
+// var teaspoon = function(tablespoon){
+//   return tablespoon * 3
+// };
+//
+// var tablespoon = parseInt(prompt("How many tablespoons do you have?"));
+// var result = teaspoon(tablespoon);
+//
+// alert("You have " + result + " teaspoons.");
+
+//UI Calculator
+
+  //Business logic
+var add = function(number1, number2) {
+  return number1 + number2;
+}
+
+var subract = function(number1, number2) {
+  return number1 - number2;
+}
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+}
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+}
+
+  //UI Logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
